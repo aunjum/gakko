39def0df27fcacb2939e480df01fc271354d8fac
aunjum <aunjum@uxd.co.jp>
2022-01-05 14:52:48 +0600

    App updated

--- App.js
+++ App.js
@@ -1,23 +1,9 @@
 import * as React from "react";
 import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
-import tw from 'tailwind-react-native-classnames'
-import * as Sentry from '@sentry/react-native';
-import {
-  AdMobBanner,
-  AdMobInterstitial,
-  PublisherBanner,
-  AdMobRewarded,
-  setTestDeviceIDAsync,
-} from 'expo-ads-admob';
 export default function App() {
   const textPress = () => console.log('Text');
   return (
     <SafeAreaView style={styles.container}>
-      <AdMobBanner
-        bannerSize="fullBanner"
-        adUnitID="ca-app-pub-1264500349297774/4900363531" // Test ID, Replace with your-admob-unit-id
-        servePersonalizedAds // true or false
-        onDidFailToReceiveAdWithError={this.bannerError} />
       <Text  onPress={textPress} style={styles.h1}>
       gakkō
       </Text>
